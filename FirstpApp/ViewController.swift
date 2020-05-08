35f5bdb6cdc49abc56319ffd53c61ef044c86f00
Pheonvova <pheon1310@gmail.com>
2020-05-08 18:30:46 +0300

    first commit try

---
+++ FirstpApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Firstp App
+//
+//  Created by Pheon on 08.05.2020.
+//  Copyright © 2020 Pheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let a = "a "
+        let b = "a "
+    }
+
+
+}
+
